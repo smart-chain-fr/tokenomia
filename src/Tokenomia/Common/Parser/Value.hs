084e894f799baad1a9d90500ee734341bb4b7376
devfull <26844641+devfull@users.noreply.github.com>
2022-04-15 10:15:27 +0200

    [token] use monoid property of Value

--- src/Tokenomia/Common/Parser/Value.hs
+++ src/Tokenomia/Common/Parser/Value.hs
@@ -2,11 +2,10 @@
 {-# LANGUAGE OverloadedStrings          #-}
 
 module Tokenomia.Common.Parser.Value
-    ( allValues
-    , value
+    ( value
     ) where
 
-import Tokenomia.Common.Parser.AssetClass qualified as P
+import Tokenomia.Common.Parser.AssetClass qualified as Parser
     ( assetClass )
 
 import Data.Attoparsec.Text
@@ -18,17 +17,16 @@ import Data.Attoparsec.Text
     )
 
 import Prelude           hiding ( take )
-import Data.List.NonEmpty       ( NonEmpty, fromList )
 
 import Plutus.V1.Ledger.Api     ( Value )
 import Ledger.Value             ( assetClassValue )
 
 
 value :: Parser Value
-value = flip assetClassValue
-    <$> signed decimal
-    <*  skipSpace
-    <*> P.assetClass
-
-allValues :: Parser (NonEmpty Value)
-allValues = fromList <$> value `sepBy1` " + "
+value = mconcat <$> singletonValue `sepBy1` " + "
+  where
+    singletonValue :: Parser Value
+    singletonValue = flip assetClassValue
+        <$> signed decimal
+        <*  skipSpace
+        <*> Parser.assetClass
