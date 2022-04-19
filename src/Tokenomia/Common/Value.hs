2531a35fadac77a9bdfdaba250bbc54414af4e10
devfull <26844641+devfull@users.noreply.github.com>
2022-04-19 10:52:09 +0200

    [token] use parsers for Value deserialisation

--- src/Tokenomia/Common/Value.hs
+++ src/Tokenomia/Common/Value.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# LANGUAGE ImportQualifiedPost #-}
 module Tokenomia.Common.Value 
   ( getTokenFrom
   , getTokensFrom
@@ -12,10 +13,7 @@ module Tokenomia.Common.Value
   , containsCollateral
   , showValueUtf8) where
 
-import Tokenomia.Common.Serialise 
-
-import qualified Data.Text as T
-import Data.Text (Text)
+import Tokenomia.Common.Serialise
 
 import Data.List as L
 
@@ -24,8 +22,12 @@ import Ledger.Ada
 import Data.String
 import Data.Foldable
 import qualified Data.Text                        as Text
+
 import qualified Data.Text.Encoding as TSE
 import Text.Hex (decodeHex)
+import Data.Attoparsec.Text (parseOnly)
+
+import Tokenomia.Common.Parser.Value qualified as Parser ( value )
 
 getTokenFrom :: Value -> (CurrencySymbol,TokenName,Integer)
 getTokenFrom  = head . filter (\(c,_,_) -> c /= adaSymbol ) .flattenValue  -- should contains only one native token (filtering ADAs) 
@@ -57,21 +59,27 @@ containingStrictlyADAs value
     = symbols value == [adaSymbol] 
 
 instance ToCLI Value where
-  toCLI = 
-    T.pack
+  toCLI =
+    Text.pack
     . fold
     . intersperse " + "
     . map (\case
             ("","",c) -> show c <> " lovelace"
+            (a,"" ,c) -> show c <> " " <> show a <> " "
+            --(a, b ,c) -> show c <> " " <> show a <> "." <> showHexadecimal b <> " "  )
             (a, b ,c) -> show c <> " " <> show a <> "." <> toString b <> " "  )
     . reverse
     . lovelacesFirst
     . flattenValue
 
+--showHexadecimal :: TokenName -> String
+--showHexadecimal = Text.unpack . encodeHex . BSU.fromString . toString
+--showHexadecimal = Text.unpack . encodeHex . fromBuiltin . unTokenName
+
 decodeToUtf8' :: TokenName -> String 
 decodeToUtf8' name 
   = case decodeHex (Text.pack $ toString name) of
-      Nothing -> error "unexpected tokenName format " <> toString name <> " should be hex."
+      Nothing -> error ("unexpected tokenName format " <> toString name <> " should be hex.")
       Just hex -> (Text.unpack . TSE.decodeUtf8) hex
 
 showValueUtf8 :: Value -> String
@@ -90,33 +98,5 @@ showValueUtf8 =
 lovelacesFirst :: [(CurrencySymbol,TokenName,Integer)] -> [(CurrencySymbol,TokenName,Integer)]
 lovelacesFirst values = let (x,y) =  span (\(c,_,_) -> c == adaSymbol) values in x <> y
 
-
-instance FromCLI Value  where
-   fromCLI = parse . tokenize
-     where 
-      parse ::  [(Text,Text,Text)] -> Value
-      parse =
-        foldMap (
-          ( \(a,b,c) -> singleton a b c) 
-          .(\(a,b,c) ->
-              ( (fromString . T.unpack )    a
-              , (fromString . T.unpack)     b
-              , (read @Integer . T.unpack)  c )))
-
-      tokenize :: Text ->  [(Text,Text,Text)]
-      tokenize 
-        = tokenizeToken
-          . removeDatum
-          . map T.words
-          . T.splitOn "+" 
-        where
-          tokenizeToken :: [[Text]] -> [(Text,Text,Text)]
-          tokenizeToken 
-            = fmap (\case
-                [a,"lovelace"] -> ("","", a)
-                [a,b] -> case T.splitOn "." b of
-                          [ph] -> (ph,"",a)
-                          [ph,tn] -> (ph,tn,a)
-                          x -> error $ "unexpected format :" <> show (T.unpack <$> x)
-                x ->  error $ "unexpected format :" <> show (T.unpack <$> x) )
-          removeDatum = L.filter (\a -> 2 == L.length a)
+instance FromCLI Value where
+    fromCLI input = either (error "") id (parseOnly Parser.value input)
