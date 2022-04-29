b51d54190a375819e7f39e74500241d9e3a3ff23
devfull <26844641+devfull@users.noreply.github.com>
2022-04-29 14:13:53 +0200

    [wallet] add various smart constructors

--- src/Tokenomia/TokenDistribution/Wallet/ChildAddress/ChildAddressRef.hs
+++ src/Tokenomia/TokenDistribution/Wallet/ChildAddress/ChildAddressRef.hs
@@ -1,12 +1,34 @@
 module Tokenomia.TokenDistribution.Wallet.ChildAddress.ChildAddressRef
     ( maxChildAddressIndexRequired
+    , defaultFeeAddressRef
+    , defaultCollateralAddressRef
     ) where
 
-import Data.List.NonEmpty ( NonEmpty )
+import Data.Composition         ( (.:) )
+import Data.List.NonEmpty       ( NonEmpty )
 
-import Tokenomia.Wallet.ChildAddress.ChildAddressRef ( ChildAddressIndex(..) )
+import Tokenomia.Wallet.Type                         ( WalletName )
 import Tokenomia.TokenDistribution.Distribution      ( Distribution )
+
+import Tokenomia.Wallet.ChildAddress.ChildAddressRef
+    ( ChildAddressIndex(..)
+    , ChildAddressRef(..)
+    , CollateralAddressRef(..)
+    , FeeAddressRef(..)
+    )
 
 
 maxChildAddressIndexRequired :: NonEmpty Distribution -> ChildAddressIndex
 maxChildAddressIndexRequired = ChildAddressIndex . fromIntegral . length
+
+feeAddressRefAtIndex :: WalletName -> ChildAddressIndex -> FeeAddressRef
+feeAddressRefAtIndex = FeeAddressRef .: ChildAddressRef
+
+collateralAddressRefAtIndex :: WalletName -> ChildAddressIndex -> CollateralAddressRef
+collateralAddressRefAtIndex = CollateralAddressRef .: ChildAddressRef
+
+defaultFeeAddressRef :: WalletName -> FeeAddressRef
+defaultFeeAddressRef = flip feeAddressRefAtIndex 0
+
+defaultCollateralAddressRef :: WalletName -> CollateralAddressRef
+defaultCollateralAddressRef = flip collateralAddressRefAtIndex 0
