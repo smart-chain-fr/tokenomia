04e69a2e915a4aa6a57df498657ad5cd96358d00
devfull <26844641+devfull@users.noreply.github.com>
2022-07-08 16:28:46 +0200

    [consolidation] use MonoTraversable in MultiAsset

--- src/Tokenomia/Common/MultiAsset.hs
+++ src/Tokenomia/Common/MultiAsset.hs
@@ -1,15 +1,35 @@
+{-# LANGUAGE DerivingVia                  #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving   #-}
 {-# LANGUAGE ImportQualifiedPost          #-}
 {-# LANGUAGE InstanceSigs                 #-}
-{-# LANGUAGE RecordWildCards              #-}
 {-# LANGUAGE TypeApplications             #-}
+{-# LANGUAGE TypeFamilies                 #-}
 
 module Tokenomia.Common.MultiAsset
     ( MultiAsset(..)
     , MultiAssetFormat(..)
     , FromValue(..)
     , ToValue(..)
-    , adjustAda
+
+    -- Value
     , groupByAssetClass
+
+    -- MultiAssetFormat
+    , getFormat
+    , singletonFormat
+    , intersection
+    , difference
+    , withoutAdaFormat
+    , restrictFormat
+    , withoutFormat
+    , restrictFormatWith
+
+    -- MultiAsset
+    , singleton
+    , withoutAda
+    , onlyAda
+    , adjustAda
+
     ) where
 
 --
@@ -36,8 +56,23 @@ module Tokenomia.Common.MultiAsset
 --
 
 import Data.List.NonEmpty       ( NonEmpty, groupAllWith )
-import Data.Map                 ( Map, fromList, toList, adjust, keysSet)
+import Data.Map.Strict          ( Map, adjust, keysSet, restrictKeys, withoutKeys )
 import Data.Set                 ( Set )
+
+import Data.Map.Strict qualified as Map
+    ( singleton
+    , fromList
+    , toList
+    )
+
+import Data.Set qualified as Set
+    ( intersection
+    , difference
+    , singleton
+    )
+
+import Data.MonoTraversable     ( MonoFunctor, Element, omap )
+import Data.Containers          ( MonoZip, ozipWith )
 
 import Ledger.Value
     ( AssetClass(..)
@@ -46,16 +81,36 @@ import Ledger.Value
     , flattenValue
     )
 
-import Tokenomia.Common.AssetClass  ( adaAssetClass )
+import Tokenomia.Common.AssetClass
+    ( adaAssetClass )
 
+import Tokenomia.Common.Data.MonoTraversable.Instance
+    ( Wrap(..) )
+
+
+-------------------------------------------------------------------------------
+-- Types
+-------------------------------------------------------------------------------
+
+type instance Element MultiAsset = Map AssetClass Integer
 
 newtype MultiAsset
     =   MultiAsset { unMultiAsset :: Map AssetClass Integer }
-    deriving (Show, Eq, Ord )
+    deriving stock   ( Show, Eq, Ord )
+    deriving newtype ( Semigroup, Monoid )
+    deriving         ( MonoFunctor, MonoZip ) via Wrap MultiAsset
+
+type instance Element MultiAssetFormat = Set AssetClass
 
 newtype MultiAssetFormat
     =   MultiAssetFormat { unMultiAssetFormat :: Set AssetClass }
-    deriving (Show, Eq, Ord)
+    deriving stock   ( Show, Eq, Ord )
+    deriving newtype ( Semigroup, Monoid)
+    deriving         ( MonoFunctor, MonoZip ) via Wrap MultiAssetFormat
+
+-------------------------------------------------------------------------------
+-- Value
+-------------------------------------------------------------------------------
 
 class FromValue a where
     fromValue :: Value -> a
@@ -63,27 +118,70 @@ class FromValue a where
 instance FromValue MultiAsset where
     fromValue :: Value -> MultiAsset
     fromValue value =
-        MultiAsset . fromList $
+        MultiAsset . Map.fromList $
             (\(cs, tn, a) -> (AssetClass (cs, tn), a)) <$> flattenValue value
 
 instance FromValue MultiAssetFormat where
     fromValue :: Value -> MultiAssetFormat
-    fromValue =
-        MultiAssetFormat . keysSet . unMultiAsset . fromValue
+    fromValue = getFormat . fromValue
 
 class ToValue a where
     toValue :: a -> Value
 
 instance ToValue MultiAsset where
     toValue :: MultiAsset -> Value
-    toValue (MultiAsset xs) =
-        mconcat $ uncurry assetClassValue <$> toList xs
+    toValue (MultiAsset m) =
+        mconcat $ uncurry assetClassValue <$> Map.toList m
 
 groupByAssetClass :: (a -> Value) -> [a] -> [NonEmpty a]
 groupByAssetClass value = groupAllWith $ fromValue @MultiAssetFormat . value
 
+-------------------------------------------------------------------------------
+-- MultiAssetFormat
+-------------------------------------------------------------------------------
+
+getFormat :: MultiAsset -> MultiAssetFormat
+getFormat = MultiAssetFormat . keysSet . unMultiAsset
+
+singletonFormat :: AssetClass -> MultiAssetFormat
+singletonFormat = MultiAssetFormat . Set.singleton
+
+intersection :: MultiAssetFormat -> MultiAssetFormat -> MultiAssetFormat
+intersection = ozipWith Set.intersection
+
+difference :: MultiAssetFormat -> MultiAssetFormat -> MultiAssetFormat
+difference = ozipWith Set.difference
+
+withoutAdaFormat :: MultiAssetFormat -> MultiAssetFormat
+withoutAdaFormat = flip difference (singletonFormat adaAssetClass)
+
+restrictFormat :: MultiAsset -> MultiAssetFormat -> MultiAsset
+restrictFormat m s =
+    MultiAsset $ unMultiAsset m `restrictKeys` unMultiAssetFormat s
+
+withoutFormat :: MultiAsset -> MultiAssetFormat -> MultiAsset
+withoutFormat m s =
+    MultiAsset $ unMultiAsset m `withoutKeys` unMultiAssetFormat s
+
+restrictFormatWith :: (a -> MultiAssetFormat -> MultiAssetFormat) -> a -> MultiAsset -> MultiAsset
+restrictFormatWith (.*.) a m =
+    let s = a .*. getFormat m
+    in  m `restrictFormat` s
+
+-------------------------------------------------------------------------------
+-- MultiAsset
+-------------------------------------------------------------------------------
+
+singleton :: AssetClass -> Integer -> MultiAsset
+singleton assetClass amount =
+    MultiAsset $ Map.singleton assetClass amount
+
+withoutAda :: MultiAsset -> MultiAsset
+withoutAda m = m `restrictFormat` withoutAdaFormat (getFormat m)
+
+onlyAda :: MultiAsset -> MultiAsset
+onlyAda m = m `restrictFormat` singletonFormat adaAssetClass
+
 adjustAda :: Integer -> MultiAsset -> MultiAsset
 adjustAda amount =
-    wrap $ adjust (const amount) adaAssetClass
-  where
-    wrap f = MultiAsset . f . unMultiAsset
+    omap $ adjust (const amount) adaAssetClass
