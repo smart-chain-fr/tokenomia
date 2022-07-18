f52eadc526d61fbf8a66c3ab55f2c45e5669a2c8
Amir Rad <ahmr10@gmail.com>
2022-07-18 11:54:04 +0100

    Refactored verify sendings and test native token verification

--- src/Tokenomia/Vesting/Sendings.hs
+++ src/Tokenomia/Vesting/Sendings.hs
@@ -28,7 +28,7 @@ import Blockfrost.Client (
 import Blockfrost.Client qualified as Client
 import Blockfrost.Lens (address, amount, outputs, txHash)
 import Control.Lens ((^.))
-import Control.Monad (unless)
+import Control.Monad (unless, when)
 import Control.Monad.Except (MonadError (throwError), liftEither)
 import Control.Monad.IO.Class (MonadIO (liftIO))
 import Control.Monad.Identity (IdentityT (IdentityT))
@@ -37,25 +37,21 @@ import Data.Aeson (FromJSON, FromJSONKey, ToJSON, ToJSONKey, eitherDecode)
 import Data.Bifunctor (first)
 import Data.ByteString.Lazy qualified as ByteString
 import Data.Default (def)
-import Data.Either.Combinators (maybeToRight)
-import Data.Foldable (find)
 import Data.Hex (unhex)
 import Data.Kind (Type)
 import Data.Map (Map)
 import Data.Map qualified as Map
 import Data.String (fromString)
 import Data.Text (pack, unpack)
-import Data.Tuple.Extra (thd3)
-import Distribution.Simple.Utils (safeHead)
 import GHC.Generics (Generic)
-import Ledger.Value (AssetClass, CurrencySymbol, TokenName, Value, assetClass, flattenValue)
+import Ledger.Value (AssetClass, CurrencySymbol, TokenName, Value, assetClass, assetClassValue, flattenValue)
 import Money qualified
 import Tokenomia.Common.Blockfrost (projectFromEnv'')
 import Tokenomia.Common.Environment (Environment)
 import Tokenomia.Common.Error (TokenomiaError (BlockFrostError))
 
 data Sendings = Sendings
-    { sendingsRecipientAddress :: Address
+  { sendingsRecipientAddress :: Address
   , sendingsTxValues :: Map TxHash Value
   }
   deriving stock (Generic, Show)
@@ -123,13 +119,15 @@ verifySendings' ::
   Sendings ->
   m ()
 verifySendings' sendings = do
-  -- TODO: maybe check if sendins is empty early to prevent unnecessary calls to Blockfrost
+  -- TODO: Create new error for sendings verification to avoid using block frost error everywhere
+  when (Map.null $ sendingsTxValues sendings) $
+    throwError . BlockFrostError . BlockfrostError $ "No sendings found"
   treasAddrTxs <- getAddressTransactions (sendingsRecipientAddress sendings)
   let treasTxhs = (^. txHash) <$> treasAddrTxs
       flatSendingsTxValues = Map.toList . sendingsTxValues $ sendings
       txhs = verifyTxHashList flatSendingsTxValues treasTxhs
   if length txhs == length flatSendingsTxValues
-    then verifyTxs sendings txhs -- this can be refactored to be called with 'sendings' only, as the above line ensures that all tx hashes in `sendings` are equivalent to tx hashes in `txhs`
+    then verifyTxs' sendings
     else
       throwError . BlockFrostError . BlockfrostError $
         "Missing Transactions"
@@ -151,40 +149,6 @@ verifyTxHashList ::
 verifyTxHashList flatSendingsTxValues treasTxhs =
   filter (`elem` treasTxhs) (fst <$> flatSendingsTxValues)
 
-verifyTxs ::
-  forall (m :: Type -> Type).
-  ( MonadRunBlockfrost m
-  , MonadError TokenomiaError m
-  ) =>
-  Sendings ->
-  [TxHash] ->
-  m ()
-verifyTxs sendings =
-  mapM_ $ verifyTx $ Map.toList $ sendingsTxValues sendings
-  where
-    verifyTx ::
-      forall (m :: Type -> Type).
-      ( MonadRunBlockfrost m
-      , MonadError TokenomiaError m
-      ) =>
-      [(TxHash, Value)] ->
-      TxHash ->
-      m ()
-    verifyTx flatTxVals txh = do
-      txValue <- liftEither (getTxValueByTxHash txh flatTxVals)
-      flatVal <- liftEither $ safeHeadToRight . flattenValue . snd $ txValue
-      bfTxUtxos <- getTxUtxos txh
-      let bfUtxoOutputs = bfTxUtxos ^. outputs
-          treasAddrOutputs =
-            filter (\output -> output ^. address == sendingsRecipientAddress sendings) bfUtxoOutputs
-          bfAmts = concat ((^. amount) <$> treasAddrOutputs)
-          bfVals = amountToAssetValue <$> bfAmts
-          totalAmount = sumRelevantValues flatVal bfVals
-          sendingsAmount = thd3 flatVal
-      unless (sendingsAmount == totalAmount) $
-        throwError . BlockFrostError . BlockfrostError $ "Values don't match "
-
--- Alternative simpler implementation. Assumes Sendings has been validated against block frost results for included recipientAddress
 verifyTxs' ::
   forall (m :: Type -> Type).
   ( MonadRunBlockfrost m
@@ -203,44 +167,27 @@ verifyTxs' sendings =
       (TxHash, Value) ->
       m ()
     verifyTx txValue = do
-      flatVal <- liftEither $ safeHeadToRight . flattenValue . snd $ txValue
+      -- Is there always a single Asset per txhash? if not then use of safeHeadToRight is not necessary
       bfTxUtxos <- getTxUtxos $ fst txValue
-      let bfUtxoOutputs = bfTxUtxos ^. outputs
-          treasAddrOutputs =
-            filter (\output -> output ^. address == sendingsRecipientAddress sendings) bfUtxoOutputs
+      let treasAddrOutputs =
+            filter
+              ((== sendingsRecipientAddress sendings) . (^. address))
+              (bfTxUtxos ^. outputs)
           bfAmts = concat ((^. amount) <$> treasAddrOutputs)
           bfVals = amountToAssetValue <$> bfAmts
-          totalAmount = sumRelevantValues flatVal bfVals
-          sendingsAmount = thd3 flatVal
-      unless (sendingsAmount == totalAmount) $
-        throwError . BlockFrostError . BlockfrostError $ "Values don't match "
 
--- This can't happen since if tx hash list is empty verifyTx in mapM_ will not
--- be called
-safeHeadToRight :: forall (a :: Type). [a] -> Either TokenomiaError a
-safeHeadToRight xs =
-  maybeToRight
-    ( BlockFrostError . BlockfrostError $
-        "List is empty"
-    )
-    (safeHead xs)
+          flatVal = fmap (\(c, t, i) -> (assetClass c t, i)) . flattenValue . snd $ txValue
+          -- Keep only relevant assets
+          bfValsFiltered = filter ((`elem` map fst flatVal) . fst) bfVals
+
+          -- Sum sendings and blockfrost totals
+          sendingsTotal = foldMap (uncurry assetClassValue) flatVal
+          bfValsTotal = foldMap (uncurry assetClassValue) bfValsFiltered
+      unless (sendingsTotal == bfValsTotal) $
+        throwError . BlockFrostError . BlockfrostError $ "Values don't match"
 
 flatValToAssetClass :: (CurrencySymbol, TokenName, Integer) -> AssetClass
 flatValToAssetClass (cs, tn, _) = assetClass cs tn
-
-sumRelevantValues :: (CurrencySymbol, TokenName, Integer) -> [(AssetClass, Integer)] -> Integer
-sumRelevantValues flatVal = foldr (\(ac, amt) z -> if ac == flatValToAssetClass flatVal then amt + z else z) 0
-
-getTxValueByTxHash ::
-  TxHash ->
-  [(TxHash, Value)] ->
-  Either TokenomiaError (TxHash, Value)
-getTxValueByTxHash txh invs =
-  maybeToRight
-    ( BlockFrostError . BlockfrostError $
-        "Investment list doesn't contain matching TxHash"
-    )
-    (find (\(t, _) -> t == txh) invs)
 
 {-
   Below, currencySymbolLength is the first part of a 2-part value returned by Money.someDiscreteCurrency,
@@ -264,6 +211,7 @@ amountToAssetValue (AssetAmount sd) =
     currencySymbol :: String
     currencySymbol = take currencySymbolLength encodedName
 
+    -- is error necessary here?
     name :: String
     name = either error id $ unhex $ drop currencySymbolLength encodedName
 
--- test/Spec/Tokenomia/Vesting/Sendings.hs
+++ test/Spec/Tokenomia/Vesting/Sendings.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
@@ -13,7 +14,7 @@
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE UndecidableInstances #-}
--- {-# OPTIONS_GHC -Werror #-}
+{-# OPTIONS_GHC -Werror #-}
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
 module Spec.Tokenomia.Vesting.Sendings (tests, main) where
@@ -22,7 +23,7 @@ import Blockfrost.Client.Types (BlockfrostError (BlockfrostNotFound))
 import Blockfrost.Types (
   Address (Address),
   AddressTransaction (AddressTransaction),
-  Amount (AdaAmount),
+  Amount (AdaAmount, AssetAmount),
   TransactionUtxos (TransactionUtxos),
   TxHash (TxHash),
   UtxoOutput (UtxoOutput),
@@ -33,12 +34,17 @@ import Control.Monad.Identity (Identity (runIdentity), IdentityT (IdentityT))
 import Control.Monad.State (MonadState, evalStateT, gets)
 import Data.ByteString.Lazy qualified as ByteString
 import Data.Either (isLeft, isRight)
+import Data.Hex (hex)
 import Data.Kind (Type)
 import Data.Map (Map)
 import Data.Map qualified as Map
-import Data.Text (Text)
+import Data.Maybe (fromJust)
+import Data.String (IsString (fromString))
+import Data.Text (Text, pack)
 import Ledger (Value)
-import Plutus.V1.Ledger.Ada (lovelaceValueOf)
+import Ledger.Ada (lovelaceValueOf)
+import Ledger.Value (CurrencySymbol (CurrencySymbol), TokenName (TokenName), assetClass, assetClassValue)
+import Money (mkSomeDiscrete, scaleFromRational)
 import Test.Tasty (TestTree, defaultMain, testGroup)
 import Test.Tasty.HUnit (assertBool, testCase)
 import Tokenomia.Common.Error (TokenomiaError (BlockFrostError))
@@ -95,7 +101,8 @@ mySendings ::
 mySendings = liftIO (ByteString.readFile "./test/Spec/Tokenomia/Vesting/sendings.json") >>= jsonToSendings
 
 testBuilder :: TestData -> Either TokenomiaError ()
-testBuilder (sendings, testState) = runIdentity $ runExceptT $ evalStateT (runFakeBlockfrost $ verifySendings' sendings) testState
+testBuilder (sendings, testState) =
+  runIdentity . runExceptT $ evalStateT (runFakeBlockfrost $ verifySendings' sendings) testState
 
 -- Missing Tx hash tests
 validTxHashTests :: TestTree
@@ -118,14 +125,11 @@ validTxHashTests =
     failTest = testBuilder $ testDataBuilder sendingsAddress sendingsTxs (bfAddrTxMap badBfAddr) bfUtxos
     badBfAddr = tail goodBfAddr
 
-    -- test data generation based on selected Blockfrost "return" value `bfAddrs`
-    {- createTestData ::  -}
     sendingsAddress = "test_address"
-    sendingsTxs = []
-    {- sendingsTxs =
+    sendingsTxs =
       [ (TxHash "abcd", lovelaceValueOf 10000000)
       , (TxHash "wxyz", lovelaceValueOf 10000000)
-      ] -}
+      ]
     bfAddrTxMap ba = [(Address sendingsAddress, ba)]
     bfUtxos = [(TxHash "abcd", abcdUtxos), (TxHash "wxyz", wxyzUtxos), (TxHash "ffff", abcdUtxos)]
     abcdUtxos = TransactionUtxos "abcd" [] outUtxos
@@ -159,18 +163,39 @@ valueCheckTests =
     failTest = testBuilder $ testDataBuilder sendingsAddress sendingsTxs bfAddrTxMap badBfUtxos
     badBfUtxos = [(TxHash "abcd", abcdUtxos), (TxHash "wxyz", abcdUtxos)]
 
-    -- test data generation based on selected Blockfrost "return" value `bfAddrs`
-    {- createTestData ::  -}
     sendingsAddress = "test_address"
-    {- sendingsTxs = [(TxHash "abcd", lovelaceValueOf 15000000), (TxHash "wxyz", lovelaceValueOf 10000000)] -}
-    sendingsTxs = [(TxHash "abcd", lovelaceValueOf 15000000), (TxHash "wxyz", lovelaceValueOf 10000000)]
-    bfAddrTxMap = [(Address sendingsAddress, [AddressTransaction (TxHash "abcd") 0 0, AddressTransaction (TxHash "wxyz") 0 0])]
+    cs = replicate 28 'a' -- 28 because hex will turn it into 56 chars
+    tk = "TOK"
+    testToken =
+      assetClassValue
+        ( assetClass
+            (CurrencySymbol $ fromString cs)
+            (TokenName $ fromString tk)
+        )
+        50
+
+    sendingsTxs =
+      [ (TxHash "abcd", lovelaceValueOf 15000000 <> testToken)
+      , (TxHash "wxyz", lovelaceValueOf 10000000)
+      ]
+    bfAddrTxMap =
+      [
+        ( Address sendingsAddress
+        ,
+          [ AddressTransaction (TxHash "abcd") 0 0
+          , AddressTransaction (TxHash "wxyz") 0 0
+          ]
+        )
+      ]
     abcdUtxos = TransactionUtxos "abcd" [] abcdOutUtxos
     wxyzUtxos = TransactionUtxos "wxyz" [] wxyzOutUtxos
     abcdOutUtxos =
       [ UtxoOutput
           (Address sendingsAddress)
-          [AdaAmount 15000000]
+          [ AdaAmount 15000000
+          , AssetAmount $
+              mkSomeDiscrete (pack (hex cs) <> pack (hex tk)) (fromJust $ scaleFromRational 1) 50
+          ]
           Nothing
           0
       ]
@@ -181,12 +206,6 @@ valueCheckTests =
           Nothing
           0
       ]
-
--- Investment list doesn't contain matching txHash
--- Given the current implementatino of `verifySendings'`, this failure csae is
--- never encountered.
-investmentListMismatch :: TestTree
-investmentListMismatch = undefined
 
 testDataBuilder ::
   Text ->
@@ -202,10 +221,3 @@ testDataBuilder sendingsAddress sendingsTxs bfAddrTxMap bfUtxos = testData
     txhValueMap = Map.fromList sendingsTxs
     -- Stuff Blockfrost will "return"
     testState = (Map.fromList bfAddrTxMap, Map.fromList bfUtxos)
-
-{- Test Cases
- - Tx count mismatch between blockfrost and input sendings
- - Sendings txs correct length but mismatich TxHashs
- - Value from sendings and blockfrost response don't match
-
--}
